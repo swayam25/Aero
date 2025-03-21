82c7dccb9266fcd83d79acd65478224c9b96941f
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-03-21 10:31:51 +0530

    Add `Tooltip`

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
     import type { UserData } from "$lib/discord/types";
+    import SolarMagniferLinear from "~icons/solar/magnifer-linear";
+    import Tooltip from "./Tooltip.svelte";
 
     let { user }: { user: UserData } = $props();
 </script>
 
-<div class="flex size-full w-20 items-center justify-center rounded-lg bg-slate-900">
-    <p class="font-bold" style="writing-mode: vertical-rl; text-orientation: upright;">SIDEBAR HERE</p>
+<div class="flex size-full w-20 items-start justify-center rounded-lg bg-slate-900 p-5 *:cursor-pointer">
+    <Tooltip side="right">
+        {#snippet trigger()}
+            <div class="size-8 opacity-80 transition-opacity hover:opacity-100">
+                <SolarMagniferLinear class="size-full" />
+            </div>
+        {/snippet}
+        {#snippet content()}
+            Search
+        {/snippet}
+    </Tooltip>
 </div>
---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { Tooltip } from "bits-ui";
+    import type { Snippet } from "svelte";
+    import { fly } from "svelte/transition";
+
+    interface Props {
+        side?: "top" | "right" | "bottom" | "left" | undefined;
+        trigger: Snippet;
+        content: Snippet;
+    }
+    let { side = undefined, trigger, content }: Props = $props();
+</script>
+
+<Tooltip.Provider>
+    <Tooltip.Root delayDuration={100}>
+        <Tooltip.Trigger>
+            {@render trigger()}
+        </Tooltip.Trigger>
+        <Tooltip.Content {side} sideOffset={8} class="z-50 rounded-lg border border-slate-700 bg-slate-900 p-2 text-sm" forceMount>
+            {#snippet child({ wrapperProps, props, open })}
+                {#if open}
+                    <div {...wrapperProps}>
+                        <div {...props} transition:fly={{ duration: 100 }}>
+                            {@render content()}
+                            <Tooltip.Arrow class="rounded-lg text-slate-700" />
+                        </div>
+                    </div>
+                {/if}
+            {/snippet}
+        </Tooltip.Content>
+    </Tooltip.Root>
+</Tooltip.Provider>
