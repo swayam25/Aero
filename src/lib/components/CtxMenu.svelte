a4fb79dd91727db113dd01bcd628ff2f5c15ea77
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-03-21 19:31:54 +0530

    Improve context menu positioning logic for better visibility

--- src/lib/components/CtxMenu.svelte
+++ src/lib/components/CtxMenu.svelte
@@ -11,13 +11,44 @@
             bodyElement.style.overflow = $store.isOpen ? "hidden" : "";
         }
     });
+
+    let ctxMenu: HTMLDivElement | null = $state(null);
+    let x: number = $state(0);
+    let y: number = $state(0);
+
+    $effect(() => {
+        if ($store.isOpen) {
+            if (ctxMenu) {
+                const menuHeight = ctxMenu.offsetHeight;
+                const menuWidth = ctxMenu.offsetWidth;
+
+                const playerElement = document.getElementById("player");
+                const playerRect = playerElement?.getBoundingClientRect();
+
+                // Adjust vertical position
+                if (window.innerHeight - $store.y < menuHeight || (playerRect && $store.y + menuHeight > playerRect.top)) {
+                    y = Math.abs($store.y - menuHeight);
+                } else {
+                    y = $store.y;
+                }
+
+                // Adjust horizontal position
+                if (window.innerWidth - $store.x < menuWidth || (playerRect && $store.x + menuWidth > playerRect.left)) {
+                    x = Math.abs($store.x - menuWidth);
+                } else {
+                    x = $store.x;
+                }
+            }
+        }
+    });
 </script>
 
 {#if $store.isOpen}
     <div
         transition:fade={{ duration: 100 }}
+        bind:this={ctxMenu}
         class="absolute z-1000 flex min-w-45 flex-col items-start justify-center rounded-lg border border-slate-700 bg-slate-900 p-2 text-sm *:flex *:w-full *:cursor-pointer *:items-center *:justify-start *:gap-2 *:rounded-lg *:px-2 *:py-1.5 *:transition-colors *:duration-200 *:hover:bg-slate-800"
-        style="top: {$store.y}px; left: {$store.x}px;"
+        style="top: {y}px; left: {x}px;"
     >
         {#if $store.type === "song" && $store.song}
             <!-- Play -->
