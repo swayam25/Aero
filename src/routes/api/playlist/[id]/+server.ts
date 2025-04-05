2eac00c4177d2f9d7369ea49c568d62572c1e862
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-04-05 11:06:43 +0530

    Remove unused import

--- src/routes/api/playlist/[id]/+server.ts
+++ src/routes/api/playlist/[id]/+server.ts
@@ -1,5 +1,5 @@
 import { addSongToPlaylist, checkPlaylist, removeSongFromPlaylist, reorderPlaylist, setPlaylistCover, toggleView } from "$lib/db";
-import { error, json, type RequestHandler } from "@sveltejs/kit";
+import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ locals, request }) => {
     const user = locals.user;
